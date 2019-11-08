7baf63cd8277a806df503d3b10143318d48b3197
ljimenez <ljimenez@futurespace.es>
2019-11-08 09:55:10 +0100

    Optimizado el algoritmo de la seguridad de la password

--- Password Generator/src/main/CheckPasswordSecurity.java
+++ Password Generator/src/main/CheckPasswordSecurity.java
@@ -1,71 +1,78 @@
 package main;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckPasswordSecurity {
     PasswordGenerator  passwordGenerator = PasswordGenerator.getInstance();
 
     public boolean check(String password){
-        boolean ok = hasAtLeastOneNumber(password);
-        if(ok){
-            ok=hasAtLeastOneUpLetter(password);
-            if(ok){
-                ok = hasAtLeastOneDownLetter(password);
-                if(ok){
-                    ok = hasAtLeastOneSpecialCharacter(password);
-                }
-            }
-        }
-        return ok;
+        return hasAtLeastOneDownLetter(password) && hasAtLeastOneNumber(password) && hasAtLeastOneSpecialCharacter(password) && hasAtLeastOneUpLetter(password);
     }
 
 
     private boolean hasAtLeastOneDownLetter(String password){
-        boolean found = false;
+       /* boolean found = false;
         int index = 0;
         while(!found && index < CharacterTypes.DOWNLETTERS.getValue().length()){
             if(password.contains(CharacterTypes.DOWNLETTERS.getValue().substring(index, index))){
                 found = true;
             }
             index++;
-        }
-        return found;
+        }*/
+        Pattern pattern = Pattern.compile("[a-z]+");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+        //return found;
     }
 
     private boolean hasAtLeastOneNumber(String password){
-        boolean found = false;
+        /*boolean found = false;
         int index = 0;
         while(!found && index < CharacterTypes.NUMBERS.getValue().length()){
             if(password.contains(CharacterTypes.NUMBERS.getValue().substring(index, index))){
                 found = true;
             }
             index++;
-        }
-        return found;
+        }*/
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+       // return found;
     }
 
 
     private boolean hasAtLeastOneSpecialCharacter(String password){
-        boolean found = false;
+        /*boolean found = false;
         int index = 0;
         while(!found && index < CharacterTypes.SPECIAL_CHARATERS.getValue().length()){
             if(password.contains(CharacterTypes.SPECIAL_CHARATERS.getValue().substring(index, index))){
                 found = true;
             }
             index++;
-        }
-        return found;
+        }*/
+        Pattern pattern = Pattern.compile("[!@#$%&¿?¡!·¬+-]+");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+        //return found;
     }
 
 
 
     private boolean hasAtLeastOneUpLetter(String password){
-        boolean found = false;
+        /*boolean found = false;
         int index = 0;
         while(!found && index < CharacterTypes.UPLETTERS.getValue().length()){
             if(password.contains(CharacterTypes.UPLETTERS.getValue().substring(index, index))){
                 found = true;
             }
             index++;
-        }
-        return found;
+        }*/
+        Pattern pattern = Pattern.compile("[A-Z]+");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+        //return found;
     }
 }
