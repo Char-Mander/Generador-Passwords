c01a97dc8018bbd0d8f865073c9b55bcc0f3f12c
ljimenez <ljimenez@futurespace.es>
2019-11-11 12:18:42 +0100

    Elminado código innecesario

--- Password Generator/src/main/Main.java
+++ Password Generator/src/main/Main.java
@@ -1,46 +1,18 @@
 package main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        GUIPassword gui = new GUIPassword(primaryStage);/*
-        Parent root = FXMLLoader.load(getClass().getResource("VentanaInicio.fxml"));
-        primaryStage.setTitle("Password Generator");
-        primaryStage.getIcons().add(new Image("/Images/lock.png"));
-        primaryStage.setScene(new Scene(root, 480, 390));
-        primaryStage.setResizable(false);
-        primaryStage.show();*/
+        GUIPassword gui = new GUIPassword(primaryStage);
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
-/*
-   public static void main(String[] args){
-        launch(args);*/
-     /*   Controller controller = new Controller();
-        controller.generatePassword();*/
-   // }
-
-/*
-    public static void main(String[] args) {
-
-        Controller controller = new Controller();
-        controller.generatePassword();
-
-    }
-
- */
 }
